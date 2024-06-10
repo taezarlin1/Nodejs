38a5138fca2e250f8625ed38b60bba98c40b7534
taezarlin1 <136051008+taezarlin1@users.noreply.github.com>
2024-06-10 13:19:50 +0630

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{"Content-type":"text/plain"});
+    res.end("Hello!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, ()=> console.log("Server is running on port 3000"));
